d2332bbf0a7aa81995c62f9d64eff1d771700807
Akshay Jain <akshay.jain@instarem.com>
2020-03-06 18:27:09 +0530

    commit

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 /**https://codeforgeek.com/file-uploads-using-node-js/ */
 var express = require("express");
+var fs = require('fs');
+var bodyParser = require('body-parser')
 var multer = require('multer');
 var cors = require('cors');
 
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
 var corsOptions = {
 	origin: '*',
 	optionsSuccessStatus: 200,
 }
-var app = express();
 app.use(cors(corsOptions))
 var storage = multer.diskStorage({
 	destination: function(req, file, callback) {
@@ -25,6 +31,12 @@ var upload = multer({storage: storage}).single('filepond');
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
+app.get('/file', function(req, res) {
+	res.sendFile(__dirname + "/file.html");
+});
+app.get('/file.js', function(req, res) {
+	res.sendFile(__dirname + "/file.js");
+});
 
 app.post('/api/photo', function(req, res) {
 	console.log('request arrived');
@@ -37,6 +49,14 @@ app.post('/api/photo', function(req, res) {
 		res.end("File is uploaded");
 	});
 });
+
+app.post('/app/64', function(req, res) {
+	var buf = new Buffer(req.body.base64String, 'base64');
+	fs.writeFile('image.png', buf, function(err) {
+		if(err) throw err;
+		console.log('Saved!');
+	});
+})
 
 app.listen(3004, function() {
 	console.log("Working on port 3004");
